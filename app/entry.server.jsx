f742321a05594d402ded7f2e8cc97a8e6898db9b
aiktsi <kate.tsiou88@gmail.com>
2025-07-02 19:45:25 +0200

    Update entry.server.jsx

--- app/entry.server.jsx
+++ app/entry.server.jsx
@@ -1,30 +1,5 @@
-import { RemixServer } from '@remix-run/react';
-import { PassThrough } from 'stream';
-import { renderToPipeableStream } from 'react-dom/server';
+import { createPagesFunctionHandler } from '@remix-run/cloudflare-pages';
+import * as build from '@remix-run/dev/server-build';
 
-export default function handleRequest(request, responseStatusCode, responseHeaders, remixContext) {
-  return new Promise((resolve, reject) => {
-    let didError = false;
-    const { pipe } = renderToPipeableStream(
-      <RemixServer context={remixContext} url={request.url} />,
-      {
-        onShellReady() {
-          const body = new PassThrough();
-          responseHeaders.set('Content-Type', 'text/html');
-          resolve(new Response(body, {
-            status: responseStatusCode,
-            headers: responseHeaders
-          }));
-          pipe(body);
-        },
-        onShellError(error) {
-          reject(error);
-        },
-        onError(error) {
-          didError = true;
-          console.error(error);
-        }
-      }
-    );
-  });
-}
+export const onRequest = createPagesFunctionHandler({ build });
+
